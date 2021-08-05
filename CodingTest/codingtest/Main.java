5fb24356a58b9eda38832c969f59cf9e9bb41ef1
Seong-Han-Sol <seoryeong1222@gmail.com>
2021-08-05 14:05:42 +0900

    커밋

---
+++ CodingTest/codingtest/Main.java
@@ -0,0 +1,35 @@
+package codingtest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*┌┬┐  8번 문제
+		  ├┼┤
+		  └┴┘
+		 * print로 출력해보기 유니코드로 출력
+		 */
+		System.out.println("\u250c\u252c\u2510\n"+"");
+	}
+}
+
+//7번 윈도우 운영체제의 파일 경로를 출력하는 연습을 해보자. 파일 경로에는 특수문자들이 포함된다. "C:\Download\hello.cpp" (단, 큰따옴표도 함께 출력한다.)
+// pritn로 백슬래시 문자 \ 를 출력하기 위해서는 \\ 로 출력해야 한다.
+//System.out.println("\""+"C:\\Download\\hello.cpp"+"\"");
+
+//6번 다음 문장을 출력하시오. "!@#$%^&*()" (단, 큰따옴표도 함께 출력한다.)
+//System.out.println("\""+"!@#$%^&*()"+"\"");
+
+//5번 print로 "Hello World"출력하기  > 이스케이프 문자 사용
+//System.out.println("\""+"Hello World"+"\"");
+
+// 4번 print로 'Hello' 출력하기
+//System.out.println("'Hello'");
+
+// 3번 print로 Hello(줄바꿈)World를 줄바꿔 출력하기
+//System.out.println("Hello"+"\n"+"World");
+
+// 2번 print로 Hello World 찍기
+//System.out.println("Hello World");
+
+// 1번 print로 Hello 찍기
+//System.out.println("Hello");
